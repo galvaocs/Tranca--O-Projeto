a836ffed321d399d2ed42b28ca4f716e40e928f8
Gabriel <gabbm9233@gmail.com>
2024-05-04 22:57:14 -0300

    alterei as rotas e vou começar a criar o js/css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/login.css">
+    <script defer src="./public/js/login.js"></script>
     <title>Document</title>
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
-Route::middleware(['auth'])->group(function () {
-    Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->group(function (){
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+    }); 
+    Route::resource('items', 'ItemController');
 });
-Route::resource('items', 'ItemController');
